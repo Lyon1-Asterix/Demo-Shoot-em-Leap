d9486c81b4caf0d6d9c94ff0661bb2118a4a1211
gus3000 <gus3000spam@gmail.com>
2016-03-17 18:02:56 +0100

    Debut LeapController

---
+++ Assets/Scripts/LeapController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapController : MonoBehaviour
+{
+    Controller controller;
+
+    void Start()
+    {
+        controller = new Controller();
+    }
+    
+    void Update()
+    {
+        Frame frame = controller.Frame();
+        Debug.Log(frame.Hands.Count);
+    }
+}
